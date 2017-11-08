6b1bf2d70455e0d162cd0ee9460483cbb9d6c630
svalee <triksrimer@gmail.com>
2017-11-08 11:52:54 +0200

    Images in now uploading to media directory

--- showcase/models.py
+++ showcase/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=50)
     order = models.PositiveIntegerField(unique=True)
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='child')
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
 
     class Meta:
         ordering = ['-order']
@@ -15,11 +15,11 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     description = models.TextField()
-    category = models.ForeignKey(Category, related_name='category')
+    category = models.ForeignKey(Category, related_name='categories')
 
     def __str__(self):
         return self.name
